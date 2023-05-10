b60d68b90030ba5aadee5461c66380de4ccfe5b0
apersonius <personius12@gmail.com>
2023-05-09 19:47:46 -0500

    add initial index.js routes/error message

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
-// routes index.js
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+router.use((req,res) => res.send('wrong route'));
+
+module.exports = router;
