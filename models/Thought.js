d3aa86421e07d54cdf57c93e527ecb76d322607c
apersonius <personius12@gmail.com>
2023-05-09 20:28:43 -0500

    add models files, add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,33 @@
-// thoughtjs
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 250
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            dafault: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MM DD, YYYY [at] hh:mm a'),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
