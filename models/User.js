9205039b6e3c77faff7663810a6ae3c813f37f0f
apersonius <personius12@gmail.com>
2023-05-09 20:59:46 -0500

    add user schema model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,44 @@
-// userjs
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            Type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'please enter vaild email address'],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
